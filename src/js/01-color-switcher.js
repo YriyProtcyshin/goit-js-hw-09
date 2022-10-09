487c8c112565b7107d9b07efa613a587ff2b4486
YriyProtcyshin <yriy.protcyshin@gmail.com>
2022-10-09 13:09:42 +0300

    task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,22 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener('click', onStartButton);
+stopBtn.addEventListener('click', onEndButton);
+
+let intervalId = null;
+
+function onStartButton() {
+  const color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  body.style.backgroundColor = color;
+  startBtn.disabled = true;
+  intervalId = setTimeout(() => {
+    onStartButton();
+  }, 2000);
+}
+
+function onEndButton() {
+  clearTimeout(intervalId);
+  startBtn.disabled = false;
+}
